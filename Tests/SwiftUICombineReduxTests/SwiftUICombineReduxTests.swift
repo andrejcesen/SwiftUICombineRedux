33540494b4ee44aab765c7af91a754c15ed18203
Andrej Česen <andrej.cesen@me.com>
2021-02-21 21:12:58 +0100

    Update tests to use `struct` for action types
    This enables using more concise syntax in epics.

--- Tests/SwiftUICombineReduxTests/SwiftUICombineReduxTests.swift
+++ Tests/SwiftUICombineReduxTests/SwiftUICombineReduxTests.swift
@@ -8,21 +8,21 @@ struct TestState: FluxState {
     var isPinging = false
 }
 
-enum AppAction: Action {
-    case increment
-    case incrementIfOdd
-    case ping
-    case pong
+struct AppAction {
+    struct increment: Action {}
+    struct incrementIfOdd: Action {}
+    struct ping: Action {}
+    struct pong: Action {}
 }
 
 func testReducer(state: TestState, action: Action) -> TestState {
     var state = state
     switch action {
-    case AppAction.increment:
+    case _ as AppAction.increment:
         state.count += 1
-    case AppAction.ping:
+    case _ as AppAction.ping:
         state.isPinging = true
-    case AppAction.pong:
+    case _ as AppAction.pong:
         state.isPinging = false
     default:
         break
@@ -38,7 +38,7 @@ struct TestView : View {
     }
     
     func onIncrementIfOdd() {
-        store.dispatch(action: AppAction.incrementIfOdd)
+        store.dispatch(action: AppAction.incrementIfOdd())
     }
     
     var body: some View {
@@ -57,33 +57,17 @@ final class SwiftUICombineReduxTests: XCTestCase {
     let store: Store<TestState> = {
         let pingEpic: Epic<TestState> = { actionPublisher, statePublisher in
             actionPublisher
-                .filter { action in
-                    if case AppAction.ping = action {
-                        return true
-                    }
-                    return false
-            }
-            .map { action in
-                AppAction.pong
-            }
-            .eraseToAnyPublisher()
+                .filter { $0 is AppAction.ping }
+                .map { _ in AppAction.pong() }
+                .eraseToAnyPublisher()
         }
         
         let incrementIfOddEpic: Epic<TestState> = { actionPublisher, statePublisher in
             actionPublisher
-                .filter { action in
-                    if case AppAction.incrementIfOdd = action {
-                        return true
-                    }
-                    return false
-            }
-            .filter { action in
-                return statePublisher.value.count % 2 == 1
-            }
-            .map { action in
-                return AppAction.increment
-            }
-            .eraseToAnyPublisher()
+                .filter { $0 is AppAction.incrementIfOdd }
+                .filter { _ in statePublisher.value.count % 2 == 1 }
+                .map { _ in AppAction.increment() }
+                .eraseToAnyPublisher()
         }
         
         let rootEpic: Epic<TestState> = { actionPublisher, statePublisher in
@@ -106,17 +90,17 @@ final class SwiftUICombineReduxTests: XCTestCase {
         
         XCTAssert(store.state.count == 0, "Initial state is not valid")
         
-        store.dispatch(action: AppAction.increment)
+        store.dispatch(action: AppAction.increment())
         DispatchQueue.main.async {
             XCTAssert(self.store.state.count == 1, "Reduced state increment is not valid")
             expectations[0].fulfill()
             
-            self.store.dispatch(action: AppAction.incrementIfOdd)
+            self.store.dispatch(action: AppAction.incrementIfOdd())
             DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
                 XCTAssert(self.store.state.count == 2, "Reduced state incrementIfOdd is not valid")
                 expectations[1].fulfill()
                 
-                self.store.dispatch(action: AppAction.incrementIfOdd)
+                self.store.dispatch(action: AppAction.incrementIfOdd())
                 DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
                     XCTAssert(self.store.state.count == 2, "Reduced state incrementIfOdd noop is not valid")
                     expectations[2].fulfill()
@@ -135,12 +119,12 @@ final class SwiftUICombineReduxTests: XCTestCase {
         XCTAssert(store.state.count == 0, "Initial state is not valid")
         
         let view: TestView = TestView(store: store)
-        store.dispatch(action: AppAction.increment)
+        store.dispatch(action: AppAction.increment())
         DispatchQueue.main.async {
             XCTAssert(view.count == 1, "Reduced state increment is not valid")
             expectations[0].fulfill()
             
-            self.store.dispatch(action: AppAction.incrementIfOdd)
+            self.store.dispatch(action: AppAction.incrementIfOdd())
             DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
                 XCTAssert(view.count == 2, "Reduced state incrementIfOdd is not valid")
                 expectations[1].fulfill()
